88ef2273f0fd944f394c8152c3cb6b7865b4e8f5
ChrisOr2022 <117246826+ChrisOr2022@users.noreply.github.com>
2025-11-03 19:53:22 -0800

    In-class Activity part 1
    We basically both jumped in at the same time and worked together as we went. Eventually we kept this up until the timer rang, and then we went out separate ways.

--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -3,5 +3,20 @@ using UnityEngine;
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
-    
+    [SerializeField] private float speed;
+    [SerializeField] Transform target;
+
+    public void Poh()
+    {
+        if (transform.position != target.position)
+        {
+            transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+        }
+       
+    }
+    public void BatFear()
+    {
+        this.enabled = false;
+    }
+
 }
